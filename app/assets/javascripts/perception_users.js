b2a2b3ce13d067793d6758a7434c6502f41087a3
asrozar <avery.rozar@insecure-it.com>
2016-10-08 08:55:19 -0400

    add perception_users javascript

---
+++ app/assets/javascripts/perception_users.js
@@ -0,0 +1,94 @@
+var PerceptionUser = function() {
+
+    var handleNewUser = function() {
+
+        $('.perception-user-form').validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            ignore: "",
+            rules: {
+
+                fullname: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                company: {
+                    required: false
+                },
+
+                username: {
+                    required: true
+                },
+                password: {
+                    required: true
+                },
+                rpassword: {
+                    equalTo: "#register_password"
+                },
+
+                tnc: {
+                    required: true
+                }
+            },
+
+            messages: { // custom messages for radio buttons and checkboxes
+                tnc: {
+                    required: "Please accept Terms."
+                }
+            },
+
+            invalidHandler: function(event, validator) { //display error alert on form submit
+
+            },
+
+            highlight: function(element) { // hightlight error inputs
+                $(element)
+                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+            },
+
+            success: function(label) {
+                label.closest('.form-group').removeClass('has-error');
+                label.remove();
+            },
+
+            errorPlacement: function(error, element) {
+                if (element.attr("name") == "tnc") { // insert checkbox errors after the container
+                    error.insertAfter($('#register_tnc_error'));
+                } else if (element.closest('.input-icon').size() === 1) {
+                    error.insertAfter(element.closest('.input-icon'));
+                } else {
+                    error.insertAfter(element);
+                }
+            },
+
+            submitHandler: function(form) {
+                form.submit();
+            }
+        });
+
+        $('.perception-user-form input').keypress(function(e) {
+            if (e.which == 13) {
+                if ($('.perception-user-form').validate().form()) {
+                    $('.perception-user-form').submit();
+                }
+                return false;
+            }
+        });
+
+    }
+
+    return {
+        //main function to initiate the module
+        init: function() {
+
+            handleNewUser();
+
+        }
+
+    };
+
+}();
