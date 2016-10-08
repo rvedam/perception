e128b8728c456cf35e98bffb9b32f0c6454d6be2
asrozar <avery.rozar@insecure-it.com>
2016-10-08 09:00:55 -0400

    add logins route and logins and perception_users resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'login', to: 'logins#new', as: 'login'
+  get 'logout', to: 'logins#destroy', as: 'logout'
+
+  resources :logins
+  resources :perception_users
+
   root :to => 'dashboards#index'
   get 'dashboards/index'
+
   get '/images/:name.:ext', :to => redirect('/assets/%{name}.%{ext}')
   get '/fonts/:name.:ext', :to => redirect('/assets/%{name}.%{ext}')
 
