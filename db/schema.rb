6eb57c6d51dc15119338c3a252c4de41f055be02
asrozar <avery.rozar@insecure-it.com>
2016-10-08 09:02:01 -0400

    update db_scheme with perception_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415153312) do
+ActiveRecord::Schema.define(version: 20161007122720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -472,6 +472,16 @@ ActiveRecord::Schema.define(version: 20160415153312) do
 
   add_index "notes", ["ntype"], name: "index_notes_on_ntype", using: :btree
   add_index "notes", ["vuln_id"], name: "index_notes_on_vuln_id", using: :btree
+
+  create_table "perception_users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "fullname"
+    t.string   "email"
+    t.string   "company"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.datetime "created_at",                null: false
